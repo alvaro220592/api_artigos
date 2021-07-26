24083993355ac84897cfdae5cd692c49e674cb6f
alvaro220592 <alvaro220592@gmail.com>
2021-07-26 15:42:50 -0300

    ...

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\ArtigoController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// List artigos
+Route::get('artigos', [ArtigoController::class, 'index']);
+
+// List single artigo
+Route::get('artigo/{id}', [ArtigoController::class, 'show']);
+
+// Create new artigo
+Route::post('artigo', [ArtigoController::class, 'store']);
+
+// Update artigo
+Route::put('artigo/{id}', [ArtigoController::class, 'update']);
+
+// Delete artigo
+Route::delete('artigo/{id}', [ArtigoController::class,'destroy']);
